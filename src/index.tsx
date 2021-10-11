e6ca0bc434113266ed6b2f2820f8f1cecf3cfc94
Alex Sharabayko <Alexander.Sharabayko@idt.net>
2021-10-11 21:45:22 +0200

    Init the project

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (): ReactElement => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
